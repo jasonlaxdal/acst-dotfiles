26ee08707152b73de197867b92ea66727d373ff9
Sean Rankin <sean@huddlehr.com>
2014-08-25 16:46:59 -0400

    Update remote_script.sh

--- remote_script.sh
+++ remote_script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Call this file using curl like this...
 # bash < <(curl -L https://raw.githubusercontent.com/srankin/dotfiles/master/remote_script.sh)
+# or 
+# curl https://raw.githubusercontent.com/srankin/dotfiles/master/remote_script.sh | bash
 echo "Here are your files:"
 ls
